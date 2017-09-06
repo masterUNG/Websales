769f1760bda9c4136097712189b0b76275c8c114
peeth <sitrach@hotmail.com>
2017-09-06 14:58:09 +0700

    Read JSON OK 3

--- app/src/main/java/pneumax/websales/fragment/AppointmentFragment.java
+++ app/src/main/java/pneumax/websales/fragment/AppointmentFragment.java
@@ -1,13 +1,16 @@
 package pneumax.websales.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import org.json.JSONArray;
@@ -15,6 +18,7 @@ import org.json.JSONObject;
 
 import pneumax.websales.R;
 import pneumax.websales.connected.GetAppointmentGridWhere;
+import pneumax.websales.manager.AppointmentAdapter;
 import pneumax.websales.manager.GlobalVar;
 import pneumax.websales.manager.MyConstant;
 import pneumax.websales.object.Employees;
@@ -88,8 +92,8 @@ public class AppointmentFragment extends Fragment {
             Log.d(tag, "JSON ==> " + resultJSON);
 
             JSONArray jsonArray = new JSONArray(resultJSON);
-            String[] AppdateStrings = new String[jsonArray.length()];
-            String[] AppStartTimeStrings = new String[jsonArray.length()];
+            final String[] AppdateStrings = new String[jsonArray.length()];
+            final String[] AppStartTimeStrings = new String[jsonArray.length()];
             String[] CSnameStrings = new String[jsonArray.length()];
             for (int i = 0; i < jsonArray.length(); i += 1) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
@@ -100,11 +104,51 @@ public class AppointmentFragment extends Fragment {
                 Log.d(tag, "Name [" + i + "] ==> " + CSnameStrings[i]);
             }//for
 
+            AppointmentAdapter appointmentAdapter = new AppointmentAdapter(getActivity(),
+                    AppdateStrings, AppStartTimeStrings, CSnameStrings);
+            listView.setAdapter(appointmentAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    confirmDialog(AppdateStrings[i], AppStartTimeStrings[i]);
+                }
+            });
+
         } catch (Exception e) {
             Log.d(tag, "e Create ListView ==> " + e.toString());
         }
 
 
     }//onActivityCreated
+
+    private void confirmDialog(String appdateString, String appStartTimeString) {
+        CharSequence[] charSequences = new CharSequence[]{"Edit", "Delete"};
+        final int[] ints = new int[]{0};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setCancelable(false);
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setTitle("Are you sure you want to Edit ?");
+        builder.setSingleChoiceItems(charSequences, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ints[0] = i + 1;
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }//ConfirmDialog
 
 }//Main Class
