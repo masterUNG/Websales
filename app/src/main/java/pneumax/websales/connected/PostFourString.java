1dacd79e3fa101219b1bf0c7e4da7b151f3930c0
peeth <sitrach@hotmail.com>
2017-09-07 14:11:06 +0700

    Last 7Sep

---
+++ app/src/main/java/pneumax/websales/connected/PostFourString.java
@@ -0,0 +1,45 @@
+package pneumax.websales.connected;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by Sitrach on 07/09/2017.
+ */
+
+public class PostFourString extends AsyncTask<String, Void, String> {
+
+    private Context context;
+    public PostFourString(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        try {
+            okhttp3.RequestBody data = new FormBody.Builder()
+                    .add(strings[0], strings[1])
+                    .add(strings[2], strings[3])
+                    .add(strings[4], strings[5])
+                    .add(strings[6], strings[7])
+                    .build();
+            okhttp3.Request.Builder builder = new okhttp3.Request.Builder();
+            builder.url(strings[8]).post(data).build();
+            okhttp3.Request request = builder.build();
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new BasicAuthInterceptor())
+                    .build();
+            okhttp3.Response response = client.newCall(request).execute();
+            String result = response.body().string();
+            return result;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
